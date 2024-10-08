baf11535201923ea2a818645935e892d66200b51
Keshav Chamria <keshavchamria@gmail.com>
2024-10-08 17:21:38 +0530

    new fixes

--- assets/built/main.js
+++ assets/built/main.js
@@ -1,2 +1,44 @@
-!function(e,t){e&&!e.getElementById("livereloadscript")&&((t=e.createElement("script")).async=1,t.src="//"+(self.location.host||"localhost").split(":")[0]+":35729/livereload.js?snipver=1",t.id="livereloadscript",e.getElementsByTagName("head")[0].appendChild(t))}(self.document),function(){"use strict";document.addEventListener("DOMContentLoaded",(function(){const e=document.getElementById("load-more");e&&e.addEventListener("click",(async function(t){t.preventDefault();const o=e.href;try{const t=await fetch(o),n=await t.text(),r=(new DOMParser).parseFromString(n,"text/html"),c=r.querySelectorAll(".gh-feed > *"),d=document.querySelector(".gh-feed"),l=document.createDocumentFragment();c.forEach((function(e){var t=document.importNode(e,!0);l.appendChild(t)})),d.appendChild(l);const a=r.querySelector(".older-posts");a&&a.href?e.href=a.href:e.remove()}catch(t){console.error("Failed to load older posts:",t)}}))}))}();
+
+(function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
+(function () {
+    'use strict';
+
+    // (function () {
+    //     pagination(true);
+    // })();
+
+    // Load More Post Logic
+    document.addEventListener('DOMContentLoaded', function () {
+      const loadMoreButton = document.getElementById('load-more');
+      if (!loadMoreButton) return;
+      loadMoreButton.addEventListener('click', async function (e) {
+        e.preventDefault(); // Prevent the default link behavior
+
+        const nextPageUrl = loadMoreButton.href;
+        try {
+          const res = await fetch(nextPageUrl);
+          const html = await res.text();
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(html, 'text/html');
+          const postElements = doc.querySelectorAll('.gh-feed > *');
+          const feedElement = document.querySelector('.gh-feed');
+          const fragment = document.createDocumentFragment();
+          postElements.forEach(function (post) {
+            var clonedItem = document.importNode(post, true);
+            fragment.appendChild(clonedItem);
+          });
+          feedElement.appendChild(fragment);
+          const resNextElement = doc.querySelector('.older-posts');
+          if (resNextElement && resNextElement.href) {
+            loadMoreButton.href = resNextElement.href;
+          } else {
+            loadMoreButton.remove();
+          }
+        } catch (e) {
+          console.error('Failed to load older posts:', e);
+        }
+      });
+    });
+
+})();
 //# sourceMappingURL=main.js.map
